021c06af442e988de1f02b633b0340d579f4e34a
IMAD-Majid <imadvlp0@gmail.com>
2024-07-10 21:33:13 +0800

    darker and simpler ui

--- sw.js
+++ sw.js
@@ -1,25 +1,40 @@
-const CACHE_VERSION = "2024-07-07 10:20 PM";
+const CACHE_VERSION = "2024-07-19 9:30 PM";
+const cf = [
+    "/",
+    "/index.html",
+    "/repo.html",
+    "/style.css",
+    "/sign.css",
+    "/main.js",
+    "/markdown-it.min.js",
+    "/sw.js",
 
+    "/icon.png",
+    "/manifest.json",
+
+    "/svg/add_white_24dp.svg",
+    "/svg/add_black_24dp.svg",
+    "/svg/indo_24dp.svg",
+    "/svg/done_white_24dp.svg",
+    "/svg/mode_edit_white_24dp.svg",
+];
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_VERSION).then((cache) => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/repo.html",
-                "/style.css",
-                "/sign.css",
-                "/main.js",
-                "/sw.js",
-
-                "/icon.png",
-                "/manifest.json",
-
-                "/svg/add_white_24dp.svg",
-                "/svg/indo_24dp.svg",
-                "/svg/done_white_24dp.svg",
-                "/svg/mode_edit_white_24dp.svg",
-            ]);
+        caches.open(CACHE_VERSION).then(async (cache) => {
+            console.log("ServiceWorker: Caching files:", cf.length, cf);
+            try {
+                cachedResult = await cache.addAll(cf);
+            } catch (err) {
+                console.error("sw: cache.addAll");
+                for (let f of cf) {
+                    try {
+                        cachedResult = await cache.add(f);
+                    } catch (err) {
+                        console.warn("sw: cache.add", f);
+                    }
+                }
+            }
+            return cachedResult;
         })
     );
 });
